99acd735e893b93793d4e6ed8facbe5f8daac9de
Depressiondetector <61242083+Aditya-Pathanjali@users.noreply.github.com>
2021-06-30 09:37:27 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -32,74 +32,74 @@ function check() {
  var result =document.getElementById('result');
  var quiz=document.getElementById('QUIZ')
  
-  if(q1=="AGREE") {a=3}
- else if(q1=="DO NOT AGREE") {a=1}
+  if(q1=="Agree") {a=3}
+ else if(q1=="Do not agree") {a=1}
  else{a=2}
  
-  if(q2=="AGREE") {b=3}
- else if(q2=="DO NOT AGREE") {b=1}
+  if(q2=="Agree") {b=3}
+ else if(q2=="Do not agree") {b=1}
  else{b=2}
 
- if(q3=="AGREE") {c=3}
- else if(q3=="DO NOT AGREE") {c=1}
+ if(q3=="Dull and Lazy") {c=3}
+ else if(q3=="Awesome") {c=1}
  else{c=2}
 
- if(q4=="AGREE") {d=3}
- else if(q4=="DO NOT AGREE") {d=1}
+ if(q4=="I Always prefer Staying at Home") {d=3}
+ else if(q4=="I Do not prefer Staying at Home") {d=1}
  else{d=2}
 
- if(q5=="AGREE") {e=3}
- else if(q5=="DO NOT AGREE") {e=1}
+ if(q5=="Agree") {e=3}
+ else if(q5=="Do not agree") {e=1}
  else{e=2}
 
- if(q6=="AGREE") {f=3}
- else if(q6=="DO NOT AGREE") {f=1}
+ if(q6=="Agree") {f=3}
+ else if(q6=="Do not agree") {f=1}
  else{f=2}
 
- if(q7=="AGREE") {g=3}
- else if(q7=="DO NOT AGREE") {g=1}
+ if(q7=="Everyday") {g=3}
+ else if(q7=="Not at all") {g=1}
  else{g=2}
 
- if(q8=="AGREE") {h=3}
- else if(q8=="DO NOT AGREE") {h=1}
+ if(q8=="Agree") {h=3}
+ else if(q8=="Do not agree") {h=1}
  else{h=2}
 
-  if(q9=="AGREE") {i=3}
- else if(q9=="DO NOT AGREE") {i=1}
+  if(q9=="No") {i=3}
+ else if(q9=="Yes") {i=1}
  else{i=2}
 
-  if(q10=="AGREE") {j=3}
- else if(q10=="DO NOT AGREE") {j=1}
+  if(q10=="No") {j=3}
+ else if(q10=="Yes") {j=1}
  else{j=2}
 
-  if(q11=="AGREE") {k=3}
- else if(q11=="DO NOT AGREE") {k=1}
+  if(q11=="Irregular sleep/ more or less than 8hrs") {k=3}
+ else if(q11=="6-8hrs") {k=1}
  else{k=2}
 
-  if(q12=="AGREE") {l=3}
- else if(q12=="DO NOT AGREE") {l=1}
+  if(q12=="No") {l=3}
+ else if(q12=="Yes") {l=1}
  else{l=2}
 
-  if(q13=="AGREE") {m=3}
- else if(q13=="DO NOT AGREE") {m=1}
+  if(q13=="Agree") {m=3}
+ else if(q13=="Do not agree") {m=1}
  else{m=2}
 
-  if(q14=="AGREE") {n=3}
- else if(q14=="DO NOT AGREE") {n=1}
+  if(q14=="Most of the time") {n=3}
+ else if(q14=="Not at all") {n=1}
  else{n=2}
 
-  if(q15=="AGREE") {o=3}
- else if(q15=="DO NOT AGREE") {o=1}
+  if(q15=="Most of the time") {o=3}
+ else if(q15=="Not at all") {o=1}
  else{o=2}
-quiz.style.display="none";
- result=a+b+c+d+e+f+g+h+i+j+k+l+m+n+o;
-  document.write("Your score is : ");
-  document.write(result);
-  document.write("\n Out of 45");
 
- if(result>=0 && result<=15) {document.write("\n\nYOU HAVE MILD DEPRESSION\n\n");} 
- else if(result>=15 && result<=30) {document.write("\n\nYOU HAVE MODERATE DEPRESSION\n\n");}
+ result=a+b+c+d+e+f+g+h+i+j+k+l+m+n+o;
+ document.write("Your Score is:   ");
+ document.write(result);
+ document.write("\n Out of 45");
+
+ if(result>=15 && result<=20) {document.write("\n"+"\n"+"YOU HAVE MILD DEPRESSION\n\n");} 
+ else if(result>=25 && result<=30) {document.write("\n"+"\n"+"YOU HAVE MODERATE DEPRESSION"+"\n"+"\n");}
 else {
-  document.write("\n\nYOU HAVE SEVERE DEPRESSION\n\n");
+  document.write("\n"+"\n"+"YOU HAVE SEVERE DEPRESSION\n\n");
 }
 } 
