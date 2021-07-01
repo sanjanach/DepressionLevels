6a3750c0484f7f85956976204ff901b5c87dd828
sanjanach <61263828+sanjanach@users.noreply.github.com>
2021-07-01 14:21:50 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -142,7 +142,7 @@ function SubmitScore()
 	}else{
 		strMessage = "<div class=\"alert alert-success\" role='alert'><b>Thank you!</h><br>Your Score is "+  score  +".</div>";
 		if(score>=0 && score<=20) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You're Perfectly Alright</h></div> "}
-		 else if(result>20 && result<=25) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You're Good. You do not have any kind of Depression</h></div> "}
+		 else if(score>20 && score<=25) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You're Good. You do not have any kind of Depression</h></div> "}
 		 else if(score>25 && score<=30) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You have Mild Depression.No need to Worry!!! Spend more time engaging in activities such as interacting with others </h></div> "}
 		 else if(score>30 && score<=35) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You Have Moderate Depression. This can be Cured by socializing or sharing your thoughts with Others.</h></div> "}
   		 else if(score>35 && score<=40) {strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>You have Severe Depression.</h></div> "}
