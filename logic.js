bcce2307314826fd2e2c2f6bcbb4c7eaa40f162d
sanjanach <61263828+sanjanach@users.noreply.github.com>
2021-08-17 19:54:41 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -170,7 +170,7 @@ function SubmitScore()
 	if(strErrMsg == '')
 	{	
 	
-			if(score == 0)
+			/*if(score == 0)
 			{
 			 strMessage = "<div class=\"alert alert-danger\" role='alert'><b>Error!</h><br>Please answer all questions.</div>";
 			}else{
@@ -179,28 +179,48 @@ function SubmitScore()
 						else if(score>20 && score<=25) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You're Good. You do not have any kind of Depression</h></div> "}
 						else if(score>25 && score<=30) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You have Mild Depression. No need to Worry!!! Spend more time engaging in activities such as interacting with others.</h></div> "}
 						else if(score>30 && score<=35) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You Have Moderate Depression. This can be Cured by socializing or sharing your thoughts with Others.<div class="alert alert-danger" role="alert">
-/* for help <a href="#" class="alert-link">click here</a>. Give it a click if you like.
-</div></h></div> "}*/
+ 				for help <a href="#" class="alert-link">click here</a>. Give it a click if you like.
+				</div></h></div> "}
 				else if(score>35 && score<=40) {strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>You have Severe Depression.</h></div> "}
 				else if(score>40 && score<=45) {strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>You Have Severe Depression</h><br>Try to Consult a Doctor if Possible</div>";}
 				else{
 						strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>You need to consult a Doctor</div>";
 					} 
-			}
+				}
 	
 	
 	
-		$("#result").html(strMessage);
-		$("#result2").html(strMessage2);
-	}else{
+			$("#result").html(strMessage);
+			$("#result2").html(strMessage2);
+		}else{
 		strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>Error!</b><ul>"+ strErrMsg +"</ul></div>";
 		$("#result").html("");
 		$("#result2").html(strMessage2);
-	}
+		}
+		*/
+	if(score == 0)
+	{
+	 strMessage = "<div class=\"alert alert-danger\" role='alert'><b>Error!</h><br>Please answer all questions.<br></div>";
+	}else{
+		strMessage = "<div class=\"alert alert-success\" role='alert'><b>Thank you!</h><br>Your Score is "+  score  +".</div>";
+         	if(score>=0 && score<=20) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You're Perfectly Alright</h></div> "}
+		 else if(result>=20 && result<=25) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You're Good. You do not have any kind of Depression</h></div> "}
+		 else if(score>=25 && score<=30) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You have Mild Depression. This can be Cured by socializing or sharing your thoughts with Others</h></div> "}
+		 else if(score>=30 && score<=35) {strMessage2 = "<div class=\"alert alert-success\" role='alert'><b>You Have Moderate Depression. No need to Worry!!! Spend more time engaging in activities such as interacting with others.</h></div> "}
+  		 else if(score>=35 && score<=40) {strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>You have Severe Depression.</h></div> "}
+    		 else if(score>=40 && score<=45) {strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>You Have Severe Depression</h><br>Try to Consult a Doctor if Possible</div>";}
+		else { strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>You need to consult a Doctor</div>";} 
+
+             }
 	
+	$("#result").html(strMessage);
+	$("#result2").html(strMessage2);
 	
-	
-}
+}else{
+		strMessage2 = "<div class=\"alert alert-danger\" role='alert'><b>Error!</b><ul>"+ strErrMsg +"</ul></div>";
+		$("#result").html("");
+		$("#result2").html(strMessage2);
+		}
 
 function resetform()
 {
